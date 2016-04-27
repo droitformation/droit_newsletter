f710d751485509c153f6e5e73d560bbe94c8dea9
Cindy Leschaud <cindy.leschaud@gmail.com>
2016-04-27 08:22:12 +0200

    Cristian fixes

--- resources/views/partials/shortcuts.blade.php
+++ resources/views/partials/shortcuts.blade.php
@@ -1,24 +1,41 @@
 <div class="container">	
     <div class="row">
-            <div class="bloc">
-            <div class="shortnav-bloc"> 
+            <!--div class="bloc">
+                <div class="shortnav-bloc"> 
+                <h4>Responsabilité civile</h4>
+                <p>Le droit de la responsabilité civile comprend l’ensemble des règles qui permettent à une personne (la victime, le lésé) de faire réparer par une tierce personne (la personne responsable) le préjudice (dommage et tort moral) subi. Les chefs d’imputation principaux sont la faute et le risque qualifié découlant de certaines installations, activités ou situations, par exemple en matière de circulation routière.</p>
+                <button type="button" class="btn btn-primary">En savoir plus</button>
+                </div>
+            </div-->
+            <div class="bloc-3-col"> 
             <h4>Responsabilité civile</h4>
-            <p>Le droit de la responsabilité civile comprend l’ensemble des règles qui permettent à une personne (la victime, le lésé) de faire réparer par une tierce personne (la personne responsable) le préjudice (dommage et tort moral) subi. Les chefs d’imputation principaux sont la faute et le risque qualifié découlant de certaines installations, activités ou situations, par exemple en matière de circulation routière.</p>
+            <p>Le droit de la responsabilité civile comprend l’ensemble des règles qui permettent à une personne de faire réparer par une tierce personne le préjudice subi.</p>
             <button type="button" class="btn btn-primary">En savoir plus</button>
+            
             </div>
-            </div>
-            <div class="bloc">
-            <div class="shortnav-bloc">
-            <h4>Assurance sociales</h4>
-            <p>Les assurances sociales représentent la protection offerte par l’Etat à tout ou partie des personnes résidant sur son territoire contre certains risques de l’existence. Les assurances sociales font l’objet de réglementations de droit public qui énoncent de manière impérative le cercle des personnes assurées, les risques assurés et les prestations offertes.</p>
-            <button type="button" class="btn btn-primary">En savoir plus</button>
-            </div></div>
-            <div class="bloc">
+            <!--div class="bloc">
+                <div class="shortnav-bloc">
+                <h4>Assurance sociales</h4>
+                <p>Les assurances sociales représentent la protection offerte par l’Etat à tout ou partie des personnes résidant sur son territoire contre certains risques de l’existence. Les assurances sociales font l’objet de réglementations de droit public qui énoncent de manière impérative le cercle des personnes assurées, les risques assurés et les prestations offertes.</p>
+                <button type="button" class="btn btn-primary">En savoir plus</button>
+                </div>
+             </div-->
+             <div class="bloc-3-col">
+                <h4>Assurance sociales</h4>
+                <p>Les assurances sociales représentent la protection offerte par l’Etat à tout ou partie des personnes résidant sur son territoire contre certains risques de l’existence.</p>
+                <button type="button" class="btn btn-primary">En savoir plus</button>
+                </div>
+            <!--div class="bloc">
             <div class="shortnav-bloc">
             <h4>Assurances privées</h4>
             <p>Sous le terme d’assurances privées, il faut entendre tout contrat conclu avec un établissement d’assurance au sens de la Loi sur la surveillance des assurances (LSA), dans le but de prémunir l’assuré contre les aléas de la réalisation d’un risque défini, moyennant paiement d’une prime.<br><br><br></p>
             <button type="button" class="btn btn-primary">En savoir plus</button>
-            </div></div>
+            </div></div-->
+            <div class="bloc-3-col">
+            <h4>Assurances privées</h4>
+            <p>Sous le terme d’assurances privées, il faut entendre tout contrat conclu avec un établissement d’assurance, dans le but de prémunir l’assuré contre un risque défini.</p>
+            <button type="button" class="btn btn-primary">En savoir plus</button>
+            </div>
 	</div>
  </div>     
     
