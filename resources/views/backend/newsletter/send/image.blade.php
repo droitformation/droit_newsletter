e478acd132478e1406334d5865f93779bbd00de0
Cindy Leschaud <cindy.leschaud@gmail.com>
2016-06-06 08:23:23 +0200

    change alt in image partials newsletter

--- resources/views/backend/newsletter/send/image.blade.php
+++ resources/views/backend/newsletter/send/image.blade.php
@@ -13,7 +13,7 @@
                         <p class="centerText">
                             <?php $lien = (isset($bloc->lien) && !empty($bloc->lien) ? $bloc->lien : url('/') ); ?>
                             <a target="_blank" href="<?php echo $lien; ?>">
-                                <img style="max-width: 560px;" alt="Droit du travail" src="{{ asset('files/'.$bloc->image) }}" />
+                                <img style="max-width: 560px;" alt="RC Assurances" src="{{ asset('files/'.$bloc->image) }}" />
                             </a>
                         </p>
                     </td>
