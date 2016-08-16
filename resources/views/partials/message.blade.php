16a57830a9988e5666d80c6fd9ed2dfc1c0e71a9
Cindy Leschaud <cindy.leschaud@gmail.com>
2016-08-16 09:23:36 +0200

    finish install new mailjet

--- resources/views/partials/message.blade.php
+++ resources/views/partials/message.blade.php
@@ -15,14 +15,12 @@
                 @endif
 
                 @if( Session::has('jeton') || Session::has('resend') )
-
                     <form action="{{ url('resend') }}" method="POST" class="form">
                         {!! csrf_field() !!}
                         <input type="hidden" value="{{ old('email') }}" name="email" />
-                        <input type="hidden" value="{{ old('newsletter_id') }}" name="newsletter_id" />
+                        <input type="hidden" value="3" name="newsletter_id" />
                         <button class="btn btn-xs btn-warning" type="submit">Renvoyer le lien d'activation</button>
-                   </form>
-
+                    </form>
                 @endif
 
             </div>
