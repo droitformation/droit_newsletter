dbe55af6215af4b635d413a08cb7ab00c1159313
Cindy Leschaud <droitformation.web@gmail.com>
2018-07-16 10:09:29 +0200

    add link analyse on frontend

--- resources/views/partials/latest.blade.php
+++ resources/views/partials/latest.blade.php
@@ -8,7 +8,11 @@
                     <?php setlocale(LC_ALL, 'fr_FR.UTF-8');  ?>
                     <span class="date">{{ $last->pub_date->formatLocalized('%d %B %Y') }}</span>
                     <a href="{{ url('jurisprudence').'/#'.$last->reference }}">{{ $last->reference }}</a>
-                    <p>{{ $last->abstract }}</p>
+                    <p style="margin-bottom: 0;">{{ $last->abstract }}</p>
+
+                    @foreach($last->analyses as $analyse)
+                        <p><a target="_blank" href="{{ asset('files/analyses/'.$analyse->file) }}">Commentaire en pdf</a></p>
+                    @endforeach
                 </li>
             @endforeach
         @endif
